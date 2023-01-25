594924544bc5845f3a8356f038552a15a8a7c432
Juan Quiroz <juan.quiroz1009@gmail.com>
2023-01-25 12:54:50 -0500

    variables-commit

--- datatype.py
+++ datatype.py
@@ -1,4 +1,31 @@
-#Tipos de Datos
-#String:
-print("dato tipo string");
-print(type("dato tipo string"));
+# Tipos de Datos
+# String:
+print("dato tipo string")
+print("dato tipo" + " string")
+
+# int
+print(type(30))
+
+# float
+print(type(30.5))
+
+# Boolean
+True
+False
+print(type(True))
+
+# List
+[1, 2, 3, 4, 5]
+print(["Juan", "Carlos"])
+[10, "hello", True, 30]
+
+# Tuples
+(10, 20, 30, 55)
+
+# Dictionaries
+print(type({"name": "juan",
+ "last-name": "quiroz",
+ "alias": "jq"}));
+
+ #None
+ 
---
+++ variables.py
@@ -0,0 +1,3 @@
+x, book = 100, "I roboot";
+print(x);
+print(book);
