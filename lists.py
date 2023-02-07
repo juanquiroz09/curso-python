dced4b7b9d46d9150c60656c3db2ad2aea44eec5
Juan Quiroz <juan.quiroz1009@gmail.com>
2023-02-07 13:52:16 -0500

    lists-commit

---
+++ lists.py
@@ -0,0 +1,17 @@
+demo_list = [1,'hello', 1.34, True, [1,2,3]]
+colors = ['red', 'green', 'blue']
+numbers_list = list((1,2,3,4))
+#print(type(numbers_list))
+#r = list(range (0, 101))
+#print(r)
+#print(len(colors))
+#print(len(demo_list))
+#print(colors[2])
+#print('green' in colors)
+
+#colors.append('violet')
+#colors.extend(['orange', 'pink'])
+colors.extend('orange', 'pink')
+
+
+print(colors)
