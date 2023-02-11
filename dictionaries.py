3b4e8b615225f5b372a0477ccde674a76afe1d22
Juan Quiroz <juan.quiroz1009@gmail.com>
2023-02-11 13:34:05 -0500

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+product = {
+  "name": "book",
+  "cuantity": 3,
+  "price" : 4.99
+}
+
+person = {
+  "first_name" : "Juan",
+  "last_name" : "Quiroz"
+}
+
+print(type(product))
+print(person.keys())
