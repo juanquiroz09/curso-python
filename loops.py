35de01ba66d04b75fb73eb384a9f954467f3b228
Juan Quiroz <juan.quiroz1009@gmail.com>
2023-02-13 16:32:14 -0500

    loops

---
+++ loops.py
@@ -0,0 +1,14 @@
+#bucle for
+foods = ['aples', 'bread', 'chese', 'milk']
+
+for food in foods:
+  if food == "chese":
+    #print("You have to buy tis")
+    continue
+  print(food)
+
+#bucle while
+count = 4
+while count <= 10:
+  count = count +1
+  print(count)
