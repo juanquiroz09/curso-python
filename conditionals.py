ea4e4086ad8fd4c2904f6c87eac03901bbbba0ec
Juan Quiroz <juan.quiroz1009@gmail.com>
2023-02-13 15:43:58 -0500

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,30 @@
+x = 40
+if x < 30:
+    print("x is less than 30")
+else:
+    print("x is mayor than 30")
+
+color = "black"
+if color == "red":
+    print("the color is red")
+elif color == "blue":
+    print("the color is blue")
+else:
+    print("any color")
+
+name = "c"
+last_name = "x"
+
+if name == "Juan":
+  if last_name == "Quiroz":
+   print("You are Juan Quiroz")
+  else:
+   print("You are not Juan Quiroz")
+else:
+  print("You are not Juan")
+
+number = 20
+if number > 2 and number  <= 22:
+  print("number is greater than two and less than or equal to twenty two")
+if number > 2 or number <= 20:
+  print("number is greater than twenty or less than twenty")
