9311fb3b5b7aac6b9bf5931b0697bdc6372d1863
Juan Quiroz <juan.quiroz1009@gmail.com>
2023-02-14 13:00:56 -0500

    modules

---
+++ modules.py
@@ -0,0 +1,9 @@
+# own modules
+# thirdy party modules
+# python modules 
+import datetime
+print(datetime.date.today())
+print(datetime.timedelta(minutes=70))
+
+from datetime import timedelta
+print(timedelta(minutes=100))
