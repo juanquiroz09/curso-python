792fd8a562db16785ab9edbfaf8750563fdf1771
Juan Quiroz <juan.quiroz1009@gmail.com>
2023-02-14 12:36:10 -0500

    functions

---
+++ functions.py
@@ -0,0 +1,13 @@
+def hello(name):
+  print("hello " + name)
+
+hello("Juan")
+
+def add(number1 , number2):
+  return number1 + number2
+
+print(add(1,2))
+
+subtract = lambda num1, num2: num1 - num2
+  
+print(subtract(30,10))
